3a4b208d740c8f45fdf0ef1aa56fa0c76498e581
Goran Hrzenjak <ghoc.hrx@gmail.com>
2015-02-13 02:22:34 +0000

    Add user following

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,13 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def avatar_for(user, options = { size: 80 })
 
-    image_tag(user.picture.url, alt: user.name, class: "gravatar")
-    
+    if (user.picture?)
+      image_tag(user.picture.url, alt: user.name, 
+        class: "avatar avatar-size-#{options[:size]}")
+    else
+      image_tag("/uploads/user/picture/basic.png", alt: user.name, 
+        class: "avatar avatar-size-#{options[:size]}")
+    end
   end
   
 end
