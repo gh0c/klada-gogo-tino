dc342d7743e394f34a997eb749fea4247e355ecc
Goran Hrzenjak <ghoc.hrx@gmail.com>
2015-02-13 00:13:58 +0000

    Admin check

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,5 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Goran",
+             email: "ghoc.hrx@gmail.com",
+             password:              "2211ghoc",
+             password_confirmation: "2211ghoc",
              admin: true)
-
-40.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
