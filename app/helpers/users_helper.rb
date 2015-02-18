793de97cffda6fabc88b74ea9242dd3a1c5fbf01
Goran Hrzenjak <ghoc.hrx@gmail.com>
2015-02-18 17:05:53 +0000

    New styles added

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,59 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-  def avatar_for(user, options = { size: 80 })
+  # Returns the avatar for the given user.
+  def avatar_for(user, options = { })
 
-    if (user.picture?)
-      image_tag(user.picture.url, alt: user.name, 
-        class: "avatar avatar-size-#{options[:size]}")
+    cs = options[:cs] || {:w => 40, :h => 40}
+    size = options[:size] || 40
+    ct = options[:ct] || false
+    custom_size = options[:custom_size] || false
+    
+    if ct
+      if custom_size
+        if (user.picture?)
+          string = 
+          "<div class = 'avatar avatar-size-custom'"+
+          "style = 'background-image:url(\"#{user.picture.url}\");" +
+          "width:#{cs[:w]}px; height:#{cs[:h]}px;'></div>"
+          return string.html_safe
+               
+        else
+          string = 
+          "<div class = 'avatar avatar-size-custom'"+
+          "style = 'background-image:url(\"/uploads/user/picture/basic.png\");" +
+          "width:#{cs[:w]}px; height:#{cs[:h]}px;'></div>"
+          return string.html_safe
+        end
+        
+      else
+        if (user.picture?)
+          string = 
+          "<div class = 'avatar avatar-size-std'"+
+          "style = 'background-image:url(\"#{user.picture.url}\")'></div>"
+          return string.html_safe
+               
+        else
+          string = 
+          "<div class = 'avatar avatar-size-std'"+
+          "style = 'background-image:url(\"/uploads/user/picture/basic.png\")'></div>"
+          return string.html_safe
+        end
+      end
+    
     else
-      image_tag("/uploads/user/picture/basic.png", alt: user.name, 
-        class: "avatar avatar-size-#{options[:size]}")
+      if (user.picture?)
+        image_tag(user.picture.url, alt: user.name, 
+          class: "logo avatar-size-#{size}" )
+             
+      else
+        image_tag("/uploads/user/picture/basic.png", alt: user.name, 
+          class: "avatar avatar-size-#{size}")
+      end
     end
   end
  
-  # Returns the for the given user.
+ 
+  # Special avatar for header1
   def header_avatar_for(user, options = { size: 80 })
 
     if (user.picture?)
